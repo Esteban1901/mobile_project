52cc0c3fbef4e55ebc6ee31f0a5124304de1f270
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-10-04 10:31:17 -0300

    add contact view and form

---
+++ lib/screens/contact.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class ContactView extends StatelessWidget {
+  const ContactView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+              ),
+              style: const TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+              style: const TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Edad',
+              ),
+              style: const TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Deseo recibir info de'),
+            SwitchListTile(
+              title: const Text('Lego Avengers',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                  )),
+              value: true,
+              onChanged: (bool value) {},
+            ),
+            SwitchListTile(
+              title: const Text('Lego Star Wars',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                  )),
+              value: true,
+              onChanged: (bool value) {},
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Deseo recibir info desde'),
+            // a date picker that has today's date as the default
+            Row(
+              children: <Widget>[
+                const Text('Desde'),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('25-05-2022',
+                          style: const TextStyle(
+                            color: Colors.lightBlue,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      IconButton(
+                        icon: const Icon(Icons.calendar_today,
+                            color: Colors.lightBlue),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Enviar solicitud de contacto'),
+              // width
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(1000, 36),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                primary: Colors.orange,
+                onPrimary: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(2)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
