445a9e98a6abc5ad04dcca49d59809f411d0ef0c
Esteban1901 <esteban.reyesl@usm.cl>
2022-10-04 10:14:49 -0300

    Cambios

--- lib/screens/certamen.dart
+++ lib/screens/certamen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class certamen extends StatelessWidget {
   @override
@@ -7,12 +8,17 @@ class certamen extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios_new),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back_ios_new),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Text('C1 DAM020-2022-2'),
+            ],
           ),
         ),
         body: const Center(
