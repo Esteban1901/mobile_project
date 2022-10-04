9a21a3e51194ad75face5ad83583b7597357a30c
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-10-04 10:14:05 -0300

    feat: add title and name on app bar

--- lib/screens/certamen.dart
+++ lib/screens/certamen.dart
@@ -7,7 +7,22 @@ class certamen extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.home),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('C1 DAM020-2022-2'),
+                    Text('Eduardo Maturana', style: TextStyle(fontSize: 14.0)),
+                  ],
+                ),
+              ]),
           leading: IconButton(
             icon: Icon(Icons.arrow_back_ios_new),
             onPressed: () {
